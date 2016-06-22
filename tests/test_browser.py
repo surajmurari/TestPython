5782adee2b7a23d40950572d9fb6566944bb16fb
surajmurari <suraj_kec@ymail.com>
2016-06-22 03:55:40 +0000

    Add files via upload

---
+++ tests/test_browser.py
@@ -0,0 +1,20 @@
+import unittest
+from mtm_selenium_client.web_user import WebUser
+
+class TestClass(unittest.TestCase):
+
+    def setUp(self):
+        self.user = WebUser()
+
+    def tearDown(self):
+        self.user.browser.quit()
+
+    def test_browser(self):
+        url = "http://amazon.com"
+        self.user.launch_url(url)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
